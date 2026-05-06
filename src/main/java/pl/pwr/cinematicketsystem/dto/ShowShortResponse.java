bf64418b388c939038f863fd1892141a4185fd3c
BartlomiejRydzak <brydzak13@gmail.com>
2026-05-06 22:48:28 +0200

    add ShowShortResponse

---
+++ src/main/java/pl/pwr/cinematicketsystem/dto/ShowShortResponse.java
@@ -0,0 +1,13 @@
+package pl.pwr.cinematicketsystem.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+public class ShowShortResponse {
+    private LocalDateTime date;
+    private Integer room;
+}
