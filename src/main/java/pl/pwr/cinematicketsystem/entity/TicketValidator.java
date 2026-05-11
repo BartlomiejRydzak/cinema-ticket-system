6f20526366bfa45493e299f01ddba370ce0734a4
BartlomiejRydzak <brydzak13@gmail.com>
2026-05-11 20:17:48 +0200

    update Entity

--- src/main/java/pl/pwr/cinematicketsystem/entity/TicketValidator.java
+++ src/main/java/pl/pwr/cinematicketsystem/entity/TicketValidator.java
@@ -3,21 +3,20 @@ package pl.pwr.cinematicketsystem.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.type.descriptor.jdbc.SmallIntJdbcType;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 public class TicketValidator {
-
     @Id
-    @SequenceGenerator(name = "TICKET_VALIDATOR_ID_GENERATOR",sequenceName = "TICKET_VALIDATOR_SEQ",allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TICKET_VALIDATOR_ID_GENERATOR")
-    private Integer id;
-
-    @Column(name = "login")
-    private String login;
+    @Column(name = "username")
+    private String username;
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "enabled")
+    private Integer enabled;
 }
