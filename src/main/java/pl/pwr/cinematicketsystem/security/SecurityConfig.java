717dc5ac71cbce9ba8c65f50e307294996a48b76
BartlomiejRydzak <brydzak13@gmail.com>
2026-05-11 19:23:45 +0200

    add SecurityConfig

---
+++ src/main/java/pl/pwr/cinematicketsystem/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package pl.pwr.cinematicketsystem.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/shows/*/seats").hasRole("TICKET_VALIDATOR")
+                .anyRequest().permitAll()
+        ).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
