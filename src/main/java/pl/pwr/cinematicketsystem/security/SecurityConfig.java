dc82481267f47228462099d27ac8998992ae782a
BartlomiejRydzak <brydzak13@gmail.com>
2026-05-12 13:57:16 +0200

    add new restricted endpoints

--- src/main/java/pl/pwr/cinematicketsystem/security/SecurityConfig.java
+++ src/main/java/pl/pwr/cinematicketsystem/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -27,9 +28,13 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.GET, "/shows/*/seats").hasRole("TICKET_VALIDATOR")
-                .anyRequest().permitAll()
-        ).httpBasic(Customizer.withDefaults());
+                        .requestMatchers(HttpMethod.GET, "/shows/*/seats").hasRole("TICKET_VALIDATOR")
+                        .requestMatchers(HttpMethod.GET, "/ticket-validators/info").hasRole("TICKET_VALIDATOR")
+                        .requestMatchers(HttpMethod.GET, "/tickets/valid").hasRole("TICKET_VALIDATOR")
+                        .requestMatchers(HttpMethod.POST, "/ticket-validators/scan-ticket").hasRole("TICKET_VALIDATOR")
+                        .anyRequest().permitAll()
+                    )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
