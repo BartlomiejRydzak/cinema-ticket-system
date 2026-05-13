8da6253b88720c307ed9fc12dc33541199495fe9
Mikołaj Pieczaba <272893@student.pwr.edu.pl>
2026-05-14 00:34:57 +0200

    allow usher to see the room number id and reserved seats on ticket validation

--- src/main/java/pl/pwr/cinematicketsystem/service/TicketValidatorServiceImpl.java
+++ src/main/java/pl/pwr/cinematicketsystem/service/TicketValidatorServiceImpl.java
@@ -1,10 +1,18 @@
 package pl.pwr.cinematicketsystem.service;
 
+import static java.util.Optional.ofNullable;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.pwr.cinematicketsystem.dto.SeatResponse;
 import pl.pwr.cinematicketsystem.dto.TicketValidatorResponse;
+import pl.pwr.cinematicketsystem.entity.Reservation;
+import pl.pwr.cinematicketsystem.entity.Room;
+import pl.pwr.cinematicketsystem.entity.Seat;
+import pl.pwr.cinematicketsystem.entity.Show;
 import pl.pwr.cinematicketsystem.entity.Ticket;
 import pl.pwr.cinematicketsystem.entity.TicketState;
 import pl.pwr.cinematicketsystem.repository.TicketRepository;
@@ -33,12 +41,41 @@ public class TicketValidatorServiceImpl implements TicketValidatorService {
         List<SeatResponse> seats = showService.getSeats(
             ticket.getShow().getId()
         );
-        Integer numberOfReservedSeats = 0;
-        for (SeatResponse seat : seats) {
-            if (seat.getReserved()) {
-                numberOfReservedSeats += 1;
-            }
-        }
+
+        List<SeatResponse> reservedSeats = new java.util.ArrayList<>();
+
+        Optional<List<Ticket>> tickets = ofNullable(ticket)
+            .map(Ticket::getReservation)
+            .map(Reservation::getTickets);
+
+        Integer numberOfReservedSeats = tickets.map(List::size).orElse(0);
+
+        tickets
+            .map(List::stream)
+            .orElseGet(Stream::empty)
+            .map(Ticket::getSeat)
+            .filter(seat -> seat != null)
+            .forEach(seat -> {
+                reservedSeats.add(
+                    SeatResponse.builder()
+                        .rowNumber(seat.getRowNumber())
+                        .seatNumber(seat.getSeatNumber())
+                        .roomId(
+                            ofNullable(seat)
+                                .map(Seat::getRoom)
+                                .map(Room::getId)
+                                .orElse(null)
+                        )
+                        .reserved(Boolean.TRUE)
+                        .build()
+                );
+            });
+
+        Integer roomId = ofNullable(ticket)
+            .map(Ticket::getShow)
+            .map(Show::getRoom)
+            .map(Room::getId)
+            .orElse(null);
 
         return TicketValidatorResponse.builder()
             .ticketCode(code)
@@ -46,6 +83,8 @@ public class TicketValidatorServiceImpl implements TicketValidatorService {
             .numberOfReservedSeats(numberOfReservedSeats)
             .showDate(ticket.getShow().getDate())
             .seats(seats)
+            .roomId(roomId)
+            .reservedSeats(reservedSeats)
             .build();
     }
 }
