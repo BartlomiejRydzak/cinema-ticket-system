4d5b7117e48c1476b00a3f3476a6baf8baa11507
Mikołaj Pieczaba <272893@student.pwr.edu.pl>
2026-05-14 00:34:57 +0200

    allow usher to see the room number id and reserved seats on ticket validation

--- src/main/java/pl/pwr/cinematicketsystem/dto/TicketValidatorResponse.java
+++ src/main/java/pl/pwr/cinematicketsystem/dto/TicketValidatorResponse.java
@@ -1,18 +1,20 @@
 package pl.pwr.cinematicketsystem.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import pl.pwr.cinematicketsystem.entity.TicketState;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Builder
 @Getter
 public class TicketValidatorResponse {
+
     private String ticketCode;
     private TicketState ticketState;
     private Integer numberOfReservedSeats;
     private LocalDateTime showDate;
     private List<SeatResponse> seats;
+    private Integer roomId;
+    private List<SeatResponse> reservedSeats;
 }
